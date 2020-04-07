c2145fa5e752cd812989152a768ed845d7192a47
xiaoming2333ca <siyi.ren21@ycdsbk12.ca>
2020-04-07 13:51:14 -0400

    Create Area of a Field.py

---
+++ Area of a Field.py
@@ -0,0 +1,5 @@
+length = int(input("Please enter the length of your property in meters: "))
+width = int(input("Please enter the width of your property in meters: "))
+
+print(f'Your property is {length * width} meter square')
+
